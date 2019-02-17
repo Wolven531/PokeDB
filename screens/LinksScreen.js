e5e1b188a4e355b983563b149815a344ea632263
Anthony Williams <anthony.williams.cs@gmail.com>
2019-02-17 14:47:04 -0700

    Clear out old links and add some TODO text

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React from 'react'
+import { ScrollView, StyleSheet, Text } from 'react-native'
 
 export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
-  };
+	static navigationOptions = {
+		title: 'Links'
+	}
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
-    );
-  }
+	render() {
+		return (
+			<ScrollView style={styles.container}>
+				<Text>TODO: Add links here</Text>
+			</ScrollView>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
+	container: {
+		flex: 1,
+		paddingTop: 15,
+		backgroundColor: '#fff'
+	}
+})
