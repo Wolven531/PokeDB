a3fd5fe434bb11f8d7ef658fc0f8eb076bd5ce4a
Anthony Williams <anthony.williams.cs@gmail.com>
2019-02-17 15:02:28 -0700

    Add basic links to LinksScreen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { ScrollView, StyleSheet, Text } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { WebBrowser } from 'expo'
+import { Ionicons } from '@expo/vector-icons'
+import Touchable from 'react-native-platform-touchable'
 
 export default class LinksScreen extends React.Component {
 	static navigationOptions = {
@@ -9,9 +12,53 @@ export default class LinksScreen extends React.Component {
 	render() {
 		return (
 			<ScrollView style={styles.container}>
-				<Text>TODO: Add links here</Text>
+				<Touchable
+					style={styles.option}
+					background={Touchable.Ripple('#ccc', false)}
+					onPress={this._handleVisitPokeDB}>
+					<View style={{ flexDirection: 'row' }}>
+						<View style={styles.optionIconContainer}>
+							<Ionicons
+								name="ios-chatboxes"
+								size={22}
+								color="#ccc"
+							/>
+						</View>
+						<View style={styles.optionText}>
+							<Text style={{}}>Visit the Pokemon DB site</Text>
+						</View>
+					</View>
+				</Touchable>
+
+				<Touchable
+					style={styles.option}
+					background={Touchable.Ripple('#ccc', false)}
+					onPress={this._handleVisitGithub}>
+					<View style={{ flexDirection: 'row' }}>
+						<View style={styles.optionIconContainer}>
+							<Ionicons
+								name="ios-chatboxes"
+								size={22}
+								color="#ccc"
+							/>
+						</View>
+						<View style={styles.optionText}>
+							<Text style={{}}>
+								Visit the Github page for this app
+							</Text>
+						</View>
+					</View>
+				</Touchable>
 			</ScrollView>
 		)
+	}
+
+	_handleVisitGithub = () => {
+		WebBrowser.openBrowserAsync('https://github.com/Wolven531/PokeDB')
+	}
+
+	_handleVisitPokeDB = () => {
+		WebBrowser.openBrowserAsync('https://pokemondb.net/pokedex/national')
 	}
 }
 
@@ -20,5 +67,19 @@ const styles = StyleSheet.create({
 		flex: 1,
 		paddingTop: 15,
 		backgroundColor: '#fff'
+	},
+	optionIconContainer: {
+		marginRight: 9
+	},
+	option: {
+		backgroundColor: '#fdfdfd',
+		paddingHorizontal: 15,
+		paddingVertical: 15,
+		borderBottomWidth: StyleSheet.hairlineWidth,
+		borderBottomColor: '#EDEDED'
+	},
+	optionText: {
+		fontSize: 15,
+		marginTop: 1
 	}
 })
